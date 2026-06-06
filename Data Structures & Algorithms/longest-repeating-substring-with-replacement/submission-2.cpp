daa809877bca16348e7ca6239b22c383f3feb227
Issam Abdoh <86783607+IssamAbdoh@users.noreply.github.com>
2026-06-06 20:15:29 +0300

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int frq[26] = {0};        
+        int left=0;
+        int max_frq=0;
+        int max_length=0;
+
+        for(int right=0;right<s.size();right++){
+
+            char right_character=s[right];
+            frq[right_character - 'A']++;
+
+            max_frq=max(max_frq,frq[right_character-'A']);
+
+            int current_length=right-left+1;
+            int required_k=current_length-max_frq;
+
+            if(required_k>k){
+                char leftCharacter=s[left];
+                frq[leftCharacter -'A']--;
+                left++;
+            }
+
+            max_length=max(max_length,right-left+1);
+        }
+
+        return max_length;
+    }
+};
